0d54f880c1c06375fae020c217b44caa340daf25
Sindhu <oceansindhu200@gmail.com>
2024-06-28 22:04:04 -0500

    refacted code to use signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from "./user-input/user-input.component";
 import type { InvestmentInput } from './investment-input.model';
@@ -11,15 +11,15 @@ import { InvestmentResultsComponent } from "./investment-results/investment-resu
     imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent]
 })
 export class AppComponent {
-
-  resutlsData?: {
+  resultsData = signal<{
     year: number,
     interest: number,
     valueEndOfYear: number,
     annualInvestment: number,
     totalInterest: number,
     totalAmountInvested: number,
-  }[];
+  }[] | undefined>(undefined);
+
   
   onCalculateInvestmentResults(data: InvestmentInput) {
       const {initialInvestment, duration, expectedReturn, annualInvestment} = data;
@@ -42,6 +42,6 @@ export class AppComponent {
         });
       }
 
-      this.resutlsData = annualData;
+      this.resultsData.set(annualData);
     }
 }
